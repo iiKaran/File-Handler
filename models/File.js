0104003aeb8e0b87439ab488cac71278471b8dc3
Karan Sehgal <104912927+iiKaran@users.noreply.github.com>
2023-06-25 22:40:54 +0530

    mail functionality added

--- models/File.js
+++ models/File.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const nodemailer = require("nodemailer")
 
 const FileSchema = new mongoose.Schema({
  name: {
@@ -18,4 +18,36 @@ const FileSchema = new mongoose.Schema({
   type:String
  }
 })
+
+FileSchema.post("save", async function(doc) {
+ try{
+     // console.log("DOC",doc)
+
+     //transporter 
+     //TODO: shift this configuration under /config folder
+     let transporter = nodemailer.createTransport({
+      service: 'gmail',
+         auth:{
+             user:process.env.MAIL_USER,
+             pass:process.env.MAIL_PASS,
+         },
+     });
+
+     //send mail 
+     let info = await transporter.sendMail({
+         from:`karankumar560k@gmail.com`,
+         to:doc.email,
+         subject: "New File Uploaded on Cloudinary",
+         html:`<h2>Hello Jee</h2> <p>File Uploaded View here: <a href="${doc.imageUrl}">${doc.url}</a> </p>`,
+     })
+     
+     console.log("INFO", info);
+
+
+ }
+ catch(error) {
+     console.error(error);
+ }
+})
+
 module.exports = mongoose.model("File",FileSchema);
